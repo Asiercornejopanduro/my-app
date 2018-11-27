c56c5b5b5644313d204063dcbee1d5d35a56506c
Curso <Curso@IPARTEKAULA.COM>
2018-11-27 13:59:52 +0100

    Nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
+import { ListadoAlumnoComponent } from './listado-alumno/listado-alumno.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'fruta', component: FrutaComponent },
+  { path: 'flujo', component: FlujoInformacionComponent },
+  { path: 'listado', component: ListadoAlumnoComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: Page404Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,18 @@ import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { ListadoAlumnoComponent } from './listado-alumno/listado-alumno.component';
 import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
+import { Page404Component } from './components/page404/page404.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FrutaComponent,
     ListadoAlumnoComponent,
-    FlujoInformacionComponent
+    FlujoInformacionComponent,
+    Page404Component,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
